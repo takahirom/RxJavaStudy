ad0ecbe29cda2f73f3f3e949cf8822197469a9e2
takahirom <takam.dev@gmail.com>
2015-04-21 00:00:00 +0900

    add variable simple hello world

--- src/main/java/part1/SimpleHelloWorld.java
+++ src/main/java/part1/SimpleHelloWorld.java
@@ -9,6 +9,16 @@ import rx.functions.Action1;
  */
 public class SimpleHelloWorld {
     public static void main(String[] args) {
+        Observable<String> myObservable =
+                Observable.just("Hello, world!");
+        Action1<String> onNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println(s);
+            }
+        };
+        myObservable.subscribe(onNextAction);
+
         Observable.just("Hello, world!")
                 .subscribe(new Action1<String>() {
                     @Override
